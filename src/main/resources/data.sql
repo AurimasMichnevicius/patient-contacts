1ce0669a9f9f76029e0ab094f7a34fd0ed62a231
Krokys9 <Krokys9@users.noreply.github.com>
2020-04-22 14:04:48 +0300

    wtf

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO patients (personalcode, condition) VALUES(12345,'Lengva');
-INSERT INTO patients (personalcode, condition) VALUES(74638,'Kritine');
-INSERT INTO patients (personalcode, condition) VALUES(87014,'Pradine');
-INSERT INTO patients (personalcode, condition) VALUES(11234,'Sunki');
